a43e7a7ff2d4fa47ee6b20be7fa1af7eeee8c3e8
Vidhika <vidhika1543@gmail.com>
2024-10-01 16:14:38 +0530

    Created the base application and given the description of SpringApplication.run(...) method

---
+++ employee-management-system-springboot/src/main/java/com/te/emsSpringBoot/EmployeeManagementSystemSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.te.emsSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementSystemSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementSystemSpringbootApplication.class, args);
+	}
+
+}
